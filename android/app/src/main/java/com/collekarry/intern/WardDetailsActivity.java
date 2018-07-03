4f6dfd71b572510780c135bbea487e4fb4c7208b
jeromenicholas <jeromenicholas07@gmail.com>
2018-07-03 15:58:24 +0530

    collapsing toolbar layout modified

--- android/app/src/main/java/com/collekarry/intern/WardDetailsActivity.java
+++ android/app/src/main/java/com/collekarry/intern/WardDetailsActivity.java
@@ -1,13 +1,32 @@
 package com.collekarry.intern;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 
-public class WardDetailsActivity extends AppCompatActivity {
+public class WardDetailsActivity extends AppCompatActivity
+//        implements TabLayout.OnTabSelectedListener,
+//        HeartRateFragment.OnFragmentInteractionListener,
+//        MedicationFragment.OnFragmentInteractionListener,
+//        AppointmentsFragment.OnFragmentInteractionListener
+{
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private WardPager adapter;
+    private AppBarLayout mAppBarLayout;
+    private CoordinatorLayout mCoordinatorLayour;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +34,39 @@ public class WardDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ward_details);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tabLayout = findViewById(R.id.tabLayout);
+        mAppBarLayout = findViewById(R.id.app_bar);
+        mCoordinatorLayour = findViewById(R.id.coordinator_layout);
+        imageView = findViewById(R.id.profilePhoto);
+
+        mAppBarLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                int heightPx = imageView.getHeight();
+                setAppBarOffset(heightPx/2);
+            }
+        });
+
+
+
+        viewPager = findViewById(R.id.pager);
+
+        adapter = new WardPager(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+//        tabLayout.addTab(tabLayout.newTab().setText("Heart rate"));
+        tabLayout.getTabAt(0).setIcon(R.drawable.heart_rate_ic);
+//        tabLayout.addTab(tabLayout.newTab().setText("Medication"));
+        tabLayout.getTabAt(1).setIcon(R.drawable.medicines_ic);
+//        tabLayout.addTab(tabLayout.newTab().setText("Appointments"));
+        tabLayout.getTabAt(2).setIcon(R.drawable.appointments_ic);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+//        tabLayout.addOnTabSelectedListener(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +77,30 @@ public class WardDetailsActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void setAppBarOffset(int offsetPx){
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) mAppBarLayout.getLayoutParams();
+        AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
+        behavior.onNestedPreScroll(mCoordinatorLayour, mAppBarLayout, null, 0, offsetPx, new int[]{0, 0}, 0);
+    }
+
+//    @Override
+//    public void onTabSelected(TabLayout.Tab tab) {
+//        viewPager.setCurrentItem(tab.getPosition());
+//    }
+//
+//    @Override
+//    public void onTabUnselected(TabLayout.Tab tab) {
+//
+//    }
+//
+//    @Override
+//    public void onTabReselected(TabLayout.Tab tab) {
+//
+//    }
+//
+//    @Override
+//    public void onFragmentInteraction(Uri uri) {
+//
+//    }
 }
---
+++ android/app/src/main/java/com/collekarry/intern/WardPager.java
@@ -0,0 +1,56 @@
+package com.collekarry.intern;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class WardPager extends FragmentPagerAdapter {
+
+    int tabCount;
+
+    public WardPager(FragmentManager fm) {
+        super(fm);
+        this.tabCount = 3;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+//                Fragment tab1 = new HeartRateFragment();
+                return HeartRateFragment.newInstance("","");
+            case 1:
+//                MedicationFragment tab2 = new MedicationFragment();
+                return MedicationFragment.newInstance("","");
+            case 2:
+//                AppointmentsFragment tab3 = new AppointmentsFragment();
+                return AppointmentsFragment.newInstance("","");
+            default :
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Heart rate";
+            case 1:
+                return "Medication";
+            case 2:
+                return "Appointments";
+            default :
+                return "";
+        }
+    }
+
+
+}
