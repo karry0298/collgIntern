c63d3e30ddb52eec4b05b3b7cc8349eab79cc747
Ashley Lobo <ashleyl@rediffmail.com>
2018-07-10 22:48:25 +0530

    structure done

--- Website/smartdevice/src/app/app-routing.module.ts
+++ Website/smartdevice/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { CaretakerComponent } from './caretaker/caretaker.component';
+import { DoctorComponent } from './doctor/doctor.component';
+import { GuardianComponent } from './guardian/guardian.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  {
+    path: 'caretaker',
+    component: CaretakerComponent
+  },
+  {
+    path: 'doctor',
+    component: DoctorComponent
+  },
+  {
+    path: 'guardian',
+    component: GuardianComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
