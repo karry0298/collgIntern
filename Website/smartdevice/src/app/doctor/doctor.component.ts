c48103dd2ec7d855597c97bbc6aece80d11034ce
Unknown <kartick.hariharan@gmail.com>
2018-07-22 02:34:50 -0400

    Auto stash before merge of "master" and "origin/master"
    bal bla commit

--- Website/smartdevice/src/app/doctor/doctor.component.ts
+++ Website/smartdevice/src/app/doctor/doctor.component.ts
@@ -8,6 +8,7 @@ import { AngularFireDatabase } from 'angularfire2/database';
 })
 export class DoctorComponent implements OnInit {
   products: any[];
+
   constructor(db: AngularFireDatabase) {
     db.list('/wards')
     .valueChanges()
