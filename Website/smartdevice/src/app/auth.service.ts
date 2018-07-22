4f7981c0c79a364a2bfd628795e6145f87aa3b03
Unknown <kartick.hariharan@gmail.com>
2018-07-22 09:21:47 -0400

    Auto stash before merge of "master" and "origin/master"

--- Website/smartdevice/src/app/auth.service.ts
+++ Website/smartdevice/src/app/auth.service.ts
@@ -1,8 +1,7 @@
 import { CanActivate, Router } from '@angular/router';
 import { AngularFireAuth } from "angularfire2/auth";
 import { Injectable } from "@angular/core";
-import { Observable } from 'rxjs'
-// import { Observable } from 'rxjs/Observable';
+import { Observable } from "rxjs";
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/take';
