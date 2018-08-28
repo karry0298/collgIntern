c52f71d463fdf614e0bcdf408695a6d57cb4eec1
Ashley Lobo <ashleyl@rediffmail.com>
2018-08-28 18:40:45 +0530

    part done

--- Website/smartdevice/src/app/doctor-medications/doctor-medications.component.ts
+++ Website/smartdevice/src/app/doctor-medications/doctor-medications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit ,Input} from '@angular/core';
 import { Patient } from '../patient';
+import { Medecine } from '../medecine';
+import * as firebase from 'firebase/app';
 @Component({
   selector: 'app-doctor-medications',
   templateUrl: './doctor-medications.component.html',
@@ -7,6 +9,13 @@ import { Patient } from '../patient';
 })
 export class DoctorMedicationsComponent implements OnInit {
   @Input() patient: Patient;
+  medicine:Medecine;
+  brandName:String;
+  
+  dateTime:Date;
+  // str1:String="";str2:String="";str3:String="";str4:String="";
+  meds:string[];
+
 display:string="none";
 
   openModal(): void{
@@ -16,6 +25,16 @@ display:string="none";
     this.display="none";
   }
   
+  submitMedsForm():any{
+    this.medicine.brandName=this.brandName;
+    this.medicine.dateStarted=this.dateTime;
+    for(var  i=0;i<4;i++){
+      if(this.meds[i]!=null){
+        this.medicine.consumptionTimings.push(this.meds[i]);
+      }
+    }
+    this.medicine.prescriptionBy="Dr."+
+  } 
   onSelect(hero: Patient): void {
     console.log(hero.medicines);
   }
@@ -24,7 +43,9 @@ display:string="none";
   //   this.selectedPatient = hero;
   //   console.log(this.selectedPatient);
   // }
-  constructor() { }
+  constructor() {
+    
+   }
   // display="none";
   //   let isDone: boolean = false; }
 
