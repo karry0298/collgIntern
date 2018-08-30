5ab989298e7883982c8aa3badeacbda2b306d511
Unknown <kartick.hariharan@gmail.com>
2018-08-30 15:20:23 +0530

    3 changes done

--- android/app/src/main/java/com/collekarry/intern/addPersonActivity.java
+++ android/app/src/main/java/com/collekarry/intern/addPersonActivity.java
@@ -62,6 +62,9 @@ public class addPersonActivity extends AppCompatActivity implements AddMedicatio
     private String userChosenTask;
     private final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
 
+    int baka=1;
+    String name="",gender="";
+    int age =-99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,13 +91,18 @@ public class addPersonActivity extends AppCompatActivity implements AddMedicatio
         medRecyclerView.setHasFixedSize(true);
 
         historyRecyclerView = findViewById(R.id.history_recycler_view);
+
+        submitButton.setClickable(true);
     }
 
 
-    public void submitButtonClicked(View v){
-        String name = nameView.getText().toString().trim();
-        int age = ageView.getText().toString().equals("") ? 0 : Integer.valueOf(ageView.getText().toString());
-        String gender;
+
+    public void submitButtonClicked(View v)
+    {
+        name = nameView.getText().toString().trim();
+        age = ageView.getText().toString().equals("") ? 0 : Integer.valueOf(ageView.getText().toString());
+
+        baka =0;
 
         if(!name.matches("^[a-zA-Z\\s]{3,20}$")){
             nameView.setError("Enter a valid name");
@@ -133,6 +141,9 @@ public class addPersonActivity extends AppCompatActivity implements AddMedicatio
 //                tempMed.add(new Medicine("MedName","BrandName",100, new Date(), t, "Dr. Kannaswmi") );
 //            }
 //            ward.setMedicines(tempMed);
+
+
+        submitButton.setClickable(false);
 
             mDatabase.child("wards").child(uid).child(key).setValue(ward)
                     .addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -394,6 +405,17 @@ public class addPersonActivity extends AppCompatActivity implements AddMedicatio
     public void onEntryComplete() {
 
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(baka != 0 || age == -99 || gender.equals("") || name.equals(""))
+        {
+            startActivity(new Intent(this,listOfPeople.class));
+        }
+    }
+
+
 
     @Override
     public void onEntryComplete(History history) {
