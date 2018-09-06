d92c9b694ae7a3a28fcca89631eeebf874a24f33
Shubham Mankar <themrshubh@gmail.com>
2018-09-06 20:12:49 +0530

    Login-database connection done
    Login is still not functional. Calm down.

--- Website/smartdevice/src/app/login/login.component.ts
+++ Website/smartdevice/src/app/login/login.component.ts
@@ -14,13 +14,10 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class LoginComponent implements OnInit {
   error: any;
   user: Observable<firebase.User>;
-  doctors:any;
+  doctors:any[];
   constructor(db: AngularFireDatabase,public afAuth: AngularFireAuth, private router: Router) {
     this.user = afAuth.authState;
-
-    db.list('/doctors').valueChanges().subscribe((val => {this.doctors = val; }))
-    console.log();
-
+    db.list('/Users/Doctors').valueChanges().subscribe((val => {this.doctors = val;}))
     // this.doctors = db.list('/doctors').push({name: "Ryan",uid: "7wfLyZWqBVVfWeFZbiDA99c04ka2", email:"Ryan@gmail.com", specialiation:"MD", phone:"9482374322" });
   }
 
@@ -30,17 +27,22 @@ export class LoginComponent implements OnInit {
     (success) => {
       var uid = firebase.auth().currentUser.uid
       var flag = false;
+      //console.log(uid);
       for(let doctor of this.doctors) {
         if(doctor.uid == uid) {
           flag = true;
           this.router.navigate(['/doctor']);
+          console.log('already reg');
           break;
         }
+        else{
+          console.log("nope");
+        }
       }
-      if (flag == false)  {
-        console.log("New User");
-        this.router.navigate(['/register']);
-      }
+      // if (flag == false)  {
+      //   //console.log("New User");
+      //   this.router.navigate(['/register']);
+      // }
 
     }).catch(
       (err) => {
@@ -52,6 +54,10 @@ export class LoginComponent implements OnInit {
   //   particlesJS.load('particles-js', 'assets/particles.json', function() {
   //   console.log('callback - particles.js config loaded');
   // });
+  }
+
+  printDoctors(){
+    console.log(this.doctors);
   }
 
 }
